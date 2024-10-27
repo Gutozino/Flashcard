bc5fd9dcc124830fbc1c1d254666117d70ef7c6c
Gutozino <gustavoalves.mello572@gmail.com>
2024-10-27 17:27:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é, a capital da China?',
+    'A capital da China é Pequim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Chinesa',
+    'Como se diz, olá em Chinês?',
+    'Se diz Nǐ hǎo'
+)
+
+criaCartao(
+    'Programação',
+    'O que e JavaScript?',
+    'É uma linguagem de programação que permite a criação de páginas web interativas e dinâmicas')
+
+
